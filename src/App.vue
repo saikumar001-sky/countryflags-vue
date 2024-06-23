69d2a9331bc7f488b11895b3ae8dc0030f889089
“sai” <sai@softdevels.com>
2024-06-23 17:38:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <span :class="flagIconClass" v-if="country"></span>
+</template>
+
+<script>
+import './styles.css'
+
+export default {
+  name: 'CountryFlag',
+  props: {
+    country: {
+      type: String,
+      required: true
+    },
+    rounded: {
+      type: Boolean,
+      default: false
+    },
+    shadow: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      validator: function (value) {
+        return value === 'small' || value === 'normal' || value === 'big'
+      },
+      default: 'normal'
+    },
+    background: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    flagIconClass() {
+      return {
+        [`flag ${this.flagIconCountry}`]: true,
+        [`rounded`]: this.rounded,
+        [`shadow`]: this.shadow,
+        [this.flagMargin]: true
+      }
+    },
+    flagIconCountry() {
+      let country = this.country.toLowerCase()
+      country = country === 'ad' ? 'andorra' : country
+      return `f-${country}`
+    },
+    flagMargin() {
+      switch (this.size) {
+        case 'small':
+          return 'small-flag'
+        case 'normal':
+          return 'normal-flag'
+        case 'big':
+          return 'big-flag'
+        default:
+          return 'normal-flag'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.small-flag {
+  margin: -0.9em -1.2em -0.9em -1.2em;
+  transform: scale(0.25);
+  -ms-transform: scale(0.25);
+  -webkit-transform: scale(0.25);
+  -moz-transform: scale(0.25);
+}
+.normal-flag {
+  margin: -0.6em -0.9em -0.6em -0.7em;
+  transform: scale(0.7);
+  -ms-transform: scale(0.7);
+  -webkit-transform: scale(0.7);
+  -moz-transform: scale(0.7);
+}
+.big-flag {
+  margin: 0;
+}
+.small-flag.rounded {
+  -moz-border-radius: 15px;
+  border-radius: 15px;
+}
+.normal-flag.rounded {
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+}
+.big-flag.rounded {
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+}
+.small-flag.shadow {
+  -webkit-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  -moz-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+}
+.normal-flag.shadow {
+  -webkit-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  -moz-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+}
+.big-flag.shadow {
+  -webkit-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  -moz-box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+  box-shadow:
+    0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 10px 0px rgb(0 0 0 / 12%);
+}
+</style>
